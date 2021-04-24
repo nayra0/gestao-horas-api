c4eb513f4ebc60b0a2cc24f65ce9f28f6228b0c0
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2021-04-24 17:53:07 -0300

    feat: Initial commit

---
+++ src/main/java/br/com/ilia/gestaohoras/gestaohoras/GestaoHorasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ilia.gestaohoras.gestaohoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoHorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ilia/gestaohoras/gestaohoras/GestaoHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ilia.gestaohoras.gestaohoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
